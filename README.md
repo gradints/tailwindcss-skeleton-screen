f30a28bf9c8d42ede747fa4cca0666015864923b
Christhofer Natalius <christhofernatalius@gmail.com>
2021-04-10 17:17:24 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # tailwindcss-skeleton-screen
+
+Tailwindcss plugin to make skeleton screen easier than ever.
+
+[Live Demo](https://jsfiddle.com)
+
+## Installation
+
+```sh
+# Using npm
+npm install -D @gradin/tailwindcss-skeleton-screen
+
+# Using Yarn
+yarn add -D @gradin/tailwindcss-skeleton-screen
+```
+
+Then add the plugin too `tailwind.config.js` file
+
+```js
+module.exports = {
+  theme: {
+    // ...
+  },
+  plugins: [
+    require('@gradints/tailwindcss-skeleton-screen'),
+    // ...
+  ],
+}
+```
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(
+  function ({ addUtilities }) {
+    addUtilities([
+      {
+        '.loading': {
+          position: 'relative',
+          overflow: 'hidden',
+          backgroundColor: '#c7c7c7',
+        },
+        '.loading::after': {
+          display: 'block',
+          position: 'absolute',
+          height: '100%',
+          top: 0,
+          left: '-10rem',
+          width: '10rem',
+          content: "''",
+          background: 'linear-gradient(to right, transparent 0%, #E8E8E8 50%, transparent 100%)',
+          animation: 'skeletonloading 1s cubic-bezier(0.4, 0.0, 0.2, 1) infinite',
+        },
+        '@keyframes skeletonloading': {
+          from: {
+            left: '-10rem',
+          },
+          to: {
+            left: '100%',
+          },
+        },
+      },
+    ])
+  }
+)
+
+// .loading {
+//   @apply bg-gray-300 overflow-hidden relative;
+//   &::after {
+//     @apply block absolute h-full top-0 -left-40 w-40;
+//     content: '';
+//     background: linear-gradient(to right, transparent 0%, #E8E8E8 50%, transparent 100%);
+//     animation: skeletonloading 1s cubic-bezier(0.4, 0.0, 0.2, 1) infinite;
+//   }
+// }
+// @keyframes skeletonloading {
+//   from {
+//     @apply -left-40;
+//   }
+//   to   {
+//     @apply left-full;
+//   }
+// }
